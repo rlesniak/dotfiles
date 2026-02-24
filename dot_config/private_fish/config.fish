93b3ec259cd4602ec29cce11ab24e1f8965eb6b3
Rafal Lesniak <rafallesniak24@gmail.com>
2026-02-24 18:50:06 +0100

    feat: add fish shell config and plugins

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,11 @@
+if status is-interactive
+    set -gx ANDROID_HOME $HOME/Library/Android/sdk
+    set -gx PATH $PATH $ANDROID_HOME/emulator
+    set -gx PATH $PATH $ANDROID_HOME/platform-tools
+end
+
+# Added by Antigravity
+fish_add_path /Users/rafal/.antigravity/antigravity/bin
+
+# Added by OrbStack
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
